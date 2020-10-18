96353280031840cc6ab47a8c9bc5188906b11b2e
Elang89 <elang8899@gmail.com>
2020-10-17 21:51:01 -0600

    add constants for errors

--- src/resource.rs
+++ src/resource.rs
@@ -1,1 +1,2 @@
-pub static ERROR_DB_ENV: &str = "DATABASE_URL not found, must be set";
+pub const ERROR_DB_ENV: &str = "DATABASE_URL not found, must be set";
+pub const ERROR_DB_CONNECTION: &str = "Could not get connection from database pool, exiting";
